9b57495e6d56ce830a634166742b661ed3417833
lifthus <lifthus531@gmail.com>
2023-01-09 22:25:15 +0900

    some common components and FirstPage routing done m

--- src/contents/main/components/FirstPage.tsx
+++ src/contents/main/components/FirstPage.tsx
@@ -4,32 +4,8 @@ import { Link } from "react-router-dom";
 
 import { useTranslation } from "react-i18next";
 
-import styled from "styled-components";
-
 import Logo from "../../../common/components/Logo";
-
-import { ThemeColor } from "../../../common/styles/theme.style";
-
-const BtnLogin = styled.button`
-  position: relative;
-
-  height: 3rem;
-  width: 10rem;
-  font-size: 1rem;
-  background-color: ${ThemeColor.basicColor};
-  color: #000766;
-  font-weight: bold;
-  border: none;
-  border-radius: 0.4rem 0.4rem 0.4rem 0.4rem;
-  transition: 0.2s;
-
-  box-shadow: 0 0 0.5rem 0.3rem;
-
-  &:hover {
-    background-color: ${ThemeColor.basicColorHover};
-    box-shadow: 0 0 0.5rem 0.5rem #3d44a7;
-  }
-`;
+import BigThemeBtn from "../../../common/components/BigThemeBtn";
 
 const FirstPage = () => {
   const { t, i18n } = useTranslation();
@@ -38,9 +14,7 @@ const FirstPage = () => {
       <Logo mov={true} />
       <p>&nbsp;</p>
       <p>&nbsp;</p>
-      <Link to="sign">
-        <BtnLogin>{t("Work out!")}</BtnLogin>
-      </Link>
+      <BigThemeBtn content={t("Work out!")} to="sign" />
     </React.Fragment>
   );
 };
