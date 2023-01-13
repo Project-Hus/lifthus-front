e8156b1408e83056fc0171de9d8f13cea5e0bd80
lifthus <lifthus531@gmail.com>
2023-01-14 00:22:11 +0900

    setting zustand

--- src/store/app.zustand.ts
+++ src/store/app.zustand.ts
@@ -9,8 +9,3 @@ const useAppStore = create<AppState>()((set) => ({
   user_id: null,
   sign_in: (id) => set((state) => ({ user_id: id })),
 }));
-
-const get_id = () => {
-  const id = useAppStore((state) => state.user_id);
-  return id;
-};
