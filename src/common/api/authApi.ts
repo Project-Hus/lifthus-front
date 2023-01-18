b7e5fcfec0bae07c9bda9e9b833c6dfc44410fad
lifthus <lifthus531@gmail.com>
2023-01-18 22:21:10 +0900

    some bug on dev env fixed and mock auth api done, api template done

--- src/common/api/authApi.ts
+++ src/common/api/authApi.ts
@@ -1,21 +1,38 @@
-interface sign_up_intf {
+export interface sign_form {
   id: string;
   password: string;
 }
 
-const authApi: any = {
-  sign_in_local: (
-    id: string,
-    password: string
-  ): { id: string; ok: boolean } => {
-    // validate it.
-    return { id: id, ok: true }; // ok is borrowed from ok idiom of Go.
+export interface sign_in_out {
+  id: string;
+  ok: boolean;
+}
+
+export interface sign_up_out {
+  fid: boolean;
+  ok: boolean;
+}
+
+const authTestApi: any = {
+  sign_in_local: ({ id, password }: sign_form): sign_in_out => {
+    let user_id = "";
+    let ok = false;
+    switch (id) {
+      case "succ":
+        if (password == id) {
+          user_id = "succ";
+          ok = true;
+        }
+        break;
+      default:
+        user_id = "";
+        ok = false;
+        break;
+    }
+    return { id: user_id, ok };
   },
 
-  sign_up_local: ({
-    id,
-    password,
-  }: sign_up_intf): { fid: boolean; ok: boolean } => {
+  sign_up_local: ({ id, password }: sign_form): sign_up_out => {
     let fid = false;
     let ok = false;
     switch (id) {
@@ -33,6 +50,21 @@ const authApi: any = {
         break;
     }
     return { fid, ok };
+  },
+};
+
+const authApi: any = {
+  sign_in_local: ({ id, password }: sign_form): sign_in_out => {
+    if (process.env.NODE_ENV == "development") {
+      return authTestApi.sign_in_local({ id, password });
+    }
+    return { id: "", ok: false };
+  },
+  sign_up_local: ({ id, password }: sign_form): sign_up_out => {
+    if (process.env.NODE_ENV == "development") {
+      return authTestApi.sign_up_local({ id, password });
+    }
+    return { fid: false, ok: false };
   },
 };
 
