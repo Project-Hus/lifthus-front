c3891c4b55dfc53dfd5cb490201e30d92a599e0b
lifthus <lifthus531@gmail.com>
2023-02-01 23:50:33 +0900

    Working on Register i18n

--- src/contents/register/RegisterBodyWeight.tsx
+++ src/contents/register/RegisterBodyWeight.tsx
@@ -1,5 +1,6 @@
 import { t } from "i18next";
 import React from "react";
+import { Trans } from "react-i18next";
 import RegisterNumber from "./components/RegisterNumber";
 
 const RegisterBodyWeight = () => {
