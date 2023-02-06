7d12f2469a5d032902d7fb117f67928b67ff5ef6
lifthus <lifthus531@gmail.com>
2023-02-07 00:28:43 +0900

    Default color theme set in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,16 +9,31 @@ import "./i18n";
 
 import { BrowserRouter } from "react-router-dom";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  baseTheme,
+  ChakraProvider,
+  withDefaultColorScheme,
+} from "@chakra-ui/react";
 
+// Setting Chakra UI's default color
 import { extendTheme } from "@chakra-ui/react";
+const theme = extendTheme(
+  {
+    colors: {
+      brand: baseTheme.colors.blue,
+    },
+  },
+  withDefaultColorScheme({
+    colorScheme: "brand",
+  })
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
