195309e7fd0dae4f1175438bbeb738618b8ee7d5
lifthus <lifthus531@gmail.com>
2023-02-09 00:42:09 +0900

    Working on Profile

--- src/contents/main/Profile.tsx
+++ src/contents/main/Profile.tsx
@@ -1,34 +1,76 @@
 import React from "react";
 
-import { Button } from "@chakra-ui/button";
-import { FormLabel } from "@chakra-ui/form-control";
-
 import useAppStore from "../../store/app.zustand";
 import styled from "@emotion/styled";
 import { Img } from "@chakra-ui/image";
 import { USER_PROFILE_IMAGE_ROUTE } from "../../common/routes";
+import { Card, CardBody, CardHeader } from "@chakra-ui/card";
+import { ThemeColor } from "../../common/styles/theme.style";
+import { Box, Heading, Stack, StackDivider, Text } from "@chakra-ui/layout";
+import { FormLabel } from "@chakra-ui/form-control";
 
 const Profile = () => {
   const user_info = useAppStore((state) => state);
   return (
     <ProfilePage>
-      <ProfileArea>
-        <div>
-          <Img
-            src={USER_PROFILE_IMAGE_ROUTE + user_info.user_id + ".jpeg"}
-            borderRadius={"2em"}
-            objectFit="cover"
-            width={"5em"}
-            height={"5em"}
-          />
-        </div>
-        <div style={{ marginTop: "auto", marginLeft: "0.5em" }}>
-          {user_info.nickname}
-        </div>
-        <div>
-          <Button> LogOut</Button>
-        </div>
-      </ProfileArea>
+      <Card
+        bgColor={ThemeColor.backgroundColor}
+        color="white"
+        border={`solid ${ThemeColor.backgroundColorLight}`}
+      >
+        <CardHeader display={"flex"}>
+          <div>
+            <Img
+              src={USER_PROFILE_IMAGE_ROUTE + user_info.user_id + ".jpeg"}
+              alt={`${user_info.nickname}'s profile image`}
+              borderRadius={"2em"}
+              objectFit={"cover"}
+              width={"7em"}
+              height={"7em"}
+            />
+          </div>
+          <FormLabel fontSize={"1.5em"} paddingLeft={"0.2em"}>
+            {user_info.nickname}
+          </FormLabel>
+        </CardHeader>
+        <CardBody></CardBody>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <Heading size="md">Client Report</Heading>
+        </CardHeader>
+
+        <CardBody>
+          <Stack divider={<StackDivider />} spacing="4">
+            <Box>
+              <Heading size="xs" textTransform="uppercase">
+                Summary
+              </Heading>
+              <Text pt="2" fontSize="sm">
+                View a summary of all your clients over the last month.
+              </Text>
+            </Box>
+            <Box>
+              <Heading size="xs" textTransform="uppercase">
+                Overview
+              </Heading>
+              <Text pt="2" fontSize="sm">
+                Check out the overview of your clients.
+              </Text>
+            </Box>
+            <Box>
+              <Heading size="xs" textTransform="uppercase">
+                Analysis
+              </Heading>
+              <Text pt="2" fontSize="sm">
+                See a detailed analysis of all your business clients.
+              </Text>
+            </Box>
+          </Stack>
+        </CardBody>
+      </Card>
+
       <div></div>
       <hr style={{ minWidth: "100wh" }} />
     </ProfilePage>
@@ -42,9 +84,11 @@ const ProfilePage = styled.div`
 `;
 
 const ProfileArea = styled.div`
+  display: flex;
   * {
-    display: flex;
-    flex-direction: row;
+    margin-bottom: auto;
+  }
+  button {
     margin-top: auto;
   }
 `;
