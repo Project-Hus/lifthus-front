bcd5b66fa6816db30ed51702351a3d49f9dfcd7c
lifthus <lifthus531@gmail.com>
2023-02-08 23:50:53 +0900

    🚧 working on Profile

--- src/contents/main/Profile.tsx
+++ src/contents/main/Profile.tsx
@@ -11,21 +11,40 @@ import { USER_PROFILE_IMAGE_ROUTE } from "../../common/routes";
 const Profile = () => {
   const user_info = useAppStore((state) => state);
   return (
-    <ProfileArea>
-      <div>
-        <Img src={USER_PROFILE_IMAGE_ROUTE + user_info.user_id + ".jpeg"}></Img>
-      </div>
-      <div>
-        {user_info.nickname}
-        <Button>adsfdsfdas</Button>
-      </div>
+    <ProfilePage>
+      <ProfileArea>
+        <div>
+          <Img
+            src={USER_PROFILE_IMAGE_ROUTE + user_info.user_id + ".jpeg"}
+            borderRadius={"2em"}
+            objectFit="cover"
+            width={"5em"}
+            height={"5em"}
+          />
+        </div>
+        <div style={{ marginTop: "auto", marginLeft: "0.5em" }}>
+          {user_info.nickname}
+        </div>
+        <div>
+          <Button> LogOut</Button>
+        </div>
+      </ProfileArea>
+      <div></div>
       <hr style={{ minWidth: "100wh" }} />
-    </ProfileArea>
+    </ProfilePage>
   );
 };
 
 export default Profile;
 
+const ProfilePage = styled.div`
+  width: 90%;
+`;
+
 const ProfileArea = styled.div`
-  width: 80%;
+  * {
+    display: flex;
+    flex-direction: row;
+    margin-top: auto;
+  }
 `;
