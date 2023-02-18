8d4bf880ee43418f466eb16ab24698f72763846d
lifthus <lifthus531@gmail.com>
2023-02-19 02:46:14 +0900

    😮 Working on Profile query and EB,Susp

--- src/contents/main/profile/Profile.tsx
+++ src/contents/main/profile/Profile.tsx
@@ -9,28 +9,46 @@ import repsApi from "../../../api/repsApi";
 import { RepContent } from "../../../api/interfacaes/repsApi.interface";
 import userApi from "../../../api/userApi";
 import { useQuery } from "@tanstack/react-query";
+import { UserId, Username } from "../../../api/interfacaes/userApi.interface";
+import { ErrorBoundary } from "react-error-boundary";
 
 const Profile = () => {
   const username = useParams().username;
 
   if (username) {
-    const { data, isLoading, isError } = useQuery({
-      queryKey: ["user_id", username],
-      queryFn: async () => {
-        const { user_id, ok } = await userApi.get_id_by_name(username);
-        const reps = await repsApi.get_user_reps(user_id);
-        if (ok) setReps(reps);
-      },
-    });
-
     const [reps, setReps] = useState<RepContent[]>([]);
 
-    const { user_id, ok } = userApi.get_id_by_name(username);
+    // Then get the user's projects
+    const {
+      status,
+      fetchStatus,
+      data: projects,
+    } = useQuery({
+      queryKey: ["projects", userId],
+      queryFn: getProjectsByUser,
+      // The query will not execute until the userId exists
+      enabled: !!userId,
+    });
+
+    const tmp: Username = { username };
+    const { data: user_id } = useQuery({
+      queryKey: ["user_id", tmp],
+      queryFn: userApi.get_id_by_name,
+    });
+
+    const { data } = useQuery({
+      queryKey: ["reps", user_id],
+      queryFn: async () => {},
+      enabled: !!user_id,
+    });
+
     if (ok)
       return (
         <BasicPageLayout>
           <ProfileCard user_id={user_id} />
-          <Reps reps={reps} />
+          <ErrorBoundary>
+            <Reps reps={reps} />
+          </ErrorBoundary>
         </BasicPageLayout>
       );
   }
