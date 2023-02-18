0090d8e919900f55e3ae9d49fdfa5c156dd28a29
lifthus <lifthus531@gmail.com>
2023-02-18 22:12:14 +0900

    reps and user Apis became promise

--- src/api/repsApi.ts
+++ src/api/repsApi.ts
@@ -2,25 +2,25 @@ import { RepContent, RepsApi } from "./interfacaes/repsApi.interface";
 import repsTestApi from "./testApi/repsTestApi";
 
 const repsApi: RepsApi = {
-  get_user_reps: (user_id: string) => {
+  get_user_reps: async (user_id: string) => {
     if (process.env.NODE_ENV === "development") {
       return repsTestApi.get_user_reps(user_id);
     }
     return repsTestApi.get_user_reps(user_id);
   },
-  post_rep: (user_id: string, rep: RepContent) => {
+  post_rep: async (user_id: string, rep: RepContent) => {
     if (process.env.NODE_ENV === "development") {
       return repsTestApi.post_rep(user_id, rep);
     }
     return repsTestApi.post_rep(user_id, rep);
   },
-  update_rep: (user_id: string, rep_id: number, rep: RepContent) => {
+  update_rep: async (user_id: string, rep_id: number, rep: RepContent) => {
     if (process.env.NODE_ENV === "development") {
       return repsTestApi.update_rep(user_id, rep_id, rep);
     }
     return repsTestApi.update_rep(user_id, rep_id, rep);
   },
-  delete_rep: (user_id: string, rep_id: number) => {
+  delete_rep: async (user_id: string, rep_id: number) => {
     if (process.env.NODE_ENV === "development") {
       return repsTestApi.delete_rep(user_id, rep_id);
     }
--- src/api/testApi/repsTestApi.ts
+++ src/api/testApi/repsTestApi.ts
@@ -3,24 +3,30 @@ import rep_list from "../mocks/repsTestApi.mocks";
 import userTestApi from "./userTestApi";
 
 const repsTestApi: RepsApi = {
-  get_user_reps: (user_id: string) => {
+  get_user_reps: async (user_id: string): Promise<RepContent[]> => {
     const list = [];
     for (const k in rep_list) {
       if (rep_list[k].user_id === user_id) {
         const next_rep = Object.assign(rep_list[k]);
-        next_rep["username"] = userTestApi.get_user_info(user_id).username;
+        next_rep["username"] = await (
+          await userTestApi.get_user_info(user_id)
+        ).username;
         list.push(next_rep);
       }
     }
     return list;
   },
-  post_rep: (user_id: string, rep: RepContent) => {
+  post_rep: async (user_id: string, rep: RepContent): Promise<boolean> => {
     return true;
   },
-  update_rep: (user_id: string, rep_id: number, rep: RepContent) => {
+  update_rep: async (
+    user_id: string,
+    rep_id: number,
+    rep: RepContent
+  ): Promise<boolean> => {
     return true;
   },
-  delete_rep: (user_id: string, rep_id: number) => {
+  delete_rep: async (user_id: string, rep_id: number): Promise<boolean> => {
     return true;
   },
 };
