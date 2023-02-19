d489358bd8cba7d782171579cae8a41de8962ff1
lifthus <lifthus531@gmail.com>
2023-02-19 21:31:30 +0900

    StatusInfo collection applied

--- src/api/testApi/authTestApi.ts
+++ src/api/testApi/authTestApi.ts
@@ -1,4 +1,5 @@
 import { AuthApi, SignParams } from "../interfacaes/authApi.interface";
+import { StatusInfo } from "../interfacaes/statusCode";
 import { UserId } from "../interfacaes/userApi.interface";
 
 import user_list from "../mocks/userTestApi.mocks";
@@ -7,20 +8,18 @@ import userTestApi from "./userTestApi";
 const authTestApi: AuthApi = {
   sign_in_local: async ({ user_id, password }: SignParams): Promise<UserId> => {
     const signInReturns: UserId = {
-      user_id: "",
+      user_id: user_id,
     };
-    if (user_id in user_list && password === "1234") {
-      signInReturns.user_id = user_id;
-    } else if (!(user_id in user_list)) {
-      return Promise.reject("no_existing_id");
-    }
-    return signInReturns;
+    if (!(user_id in user_list))
+      return Promise.reject(StatusInfo.fail.NotAcceptable);
+    else if (user_id in user_list && password === "1234") return signInReturns;
+    else return Promise.reject(StatusInfo.fail.Unauthorized.message);
   },
 
   sign_up_local: async ({ user_id, password }: SignParams): Promise<UserId> => {
     const signUpReturns: UserId = { user_id };
     if (user_id in user_list)
-      return new Promise<UserId>((_, reject) => reject("existing_id"));
+      return Promise.reject(StatusInfo.fail.Conflict.message);
     else {
       userTestApi.set_user_info({
         user_id,
