4a967d945ffd6f640d1f6b25c4632a92ffb494fb
lifthus <lifthus531@gmail.com>
2023-04-11 20:10:41 +0900

    none

--- src/contents/main/reps/components/comment.tsx
+++ src/contents/main/reps/components/comment.tsx
@@ -1,31 +1,27 @@
-import { CommentContent } from "../../../../api/interfaces/commentApi.interface"
-import { Card } from "@chakra-ui/react"
-import styled from "@emotion/styled"
-import { Text } from "@chakra-ui/layout"
+import { CommentContent } from "../../../../api/interfaces/commentApi.interface";
+import { Card } from "@chakra-ui/react";
+import styled from "@emotion/styled";
+import { Text } from "@chakra-ui/layout";
 const Comment = ({ comment }: { comment: CommentContent }) => {
-
-    const CommentBoard = styled.div`
-  border: 2px solid black;
-  border-radius: 5px;
+  const CommentBoard = styled.div`
+    border: 2px solid black;
+    border-radius: 5px;
     padding: 10px;
+  `;
+  const user_id = comment.user_id;
+  const created_at = comment.created_at;
+  return (
+    <CommentBoard>
+      <Card>
+        <Text as="b">
+          {user_id}-{created_at.toString()}-
+        </Text>
 
-`;
-    const user_id = comment.user_id
-    const created_at = comment.created_at
-    return (
-        <CommentBoard>
-            <Card>
+        <br />
+        <Text>{comment.text}</Text>
+      </Card>
+    </CommentBoard>
+  );
+};
 
-                <Text as="b">{user_id}-{created_at.toString()}-</Text>
-
-
-                <br />
-                <Text>{comment.text}</Text>
-            </Card>
-        </CommentBoard>
-    )
-}
-
-
-
-export default Comment
+export default Comment;
