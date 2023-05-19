ad74a28ce2ae619675c3af99d28f0a9b059c9407
lifthus <lifthus531@gmail.com>
2023-05-19 19:24:34 +0900

    #24/feature/getUserPosts api done

--- src/api/interfaces/repsApi.interface.ts
+++ src/api/interfaces/repsApi.interface.ts
@@ -1,11 +1,26 @@
-import { UserId } from "./userApi.interface";
+import { UserId, UserName } from "./userApi.interface";
 
 export interface RepsApi {
   get_user_reps: ({ user_id }: UserId) => Promise<RepContent[]>;
+  getUserPosts: ({ username, skip }: GetUserPostsParams) => Promise<Post[]>;
   post_rep: ({ user_id, rep }: PostRepParams) => Promise<UserId>;
   update_rep: ({ user_id, rep_id, rep }: UpdateRepParams) => Promise<UserId>;
   delete_rep: ({ uid, pid }: DeleteRepParams) => Promise<UserId>;
 }
+
+export type Post = {
+  id: number;
+  created_at: Date;
+  updated_at: Date;
+  aid: number;
+  image: string;
+  content: string;
+};
+
+export type GetUserPostsParams = {
+  username: string;
+  skip?: number;
+};
 export interface RepContent {
   rep_id: number;
   created_at: Date;
