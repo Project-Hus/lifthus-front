cd183eaab44704b7755ea40b2578fe268fad348b
lifthus <lifthus531@gmail.com>
2023-05-20 16:35:44 +0900

    #26/feat/authTestApi done

---
+++ src/api/mocks/state.mcok.ts
@@ -0,0 +1,10 @@
+export let signedUid: number | undefined = undefined;
+export let signedUsername: string | undefined = undefined;
+
+export const SigningState: {
+  uid: number | undefined;
+  username: string | undefined;
+} = {
+  uid: undefined,
+  username: undefined,
+};
