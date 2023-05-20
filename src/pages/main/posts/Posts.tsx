f45a040c869562a66810316e3a2e29c914ebcbe6
lifthus <lifthus531@gmail.com>
2023-05-20 22:52:02 +0900

    #26/feat/Posts refactored

--- src/pages/main/posts/Posts.tsx
+++ src/pages/main/posts/Posts.tsx
@@ -1,19 +1,23 @@
 import styled from "@emotion/styled";
 import React from "react";
-import Rep from "./components/Post";
+import { QueryPostDto } from "../../../api/dtos/post.dto";
+import Post from "./components/Post";
 
-const Posts = ({ reps }: { reps: RepContent[] }) => {
-  const rep_list = [];
-  for (const rep of reps) {
-    rep_list.push(<Rep rep={rep}></Rep>);
+interface PostsProps {
+  posts: QueryPostDto[];
+}
+const Posts = ({ posts }: PostsProps) => {
+  const postList = [];
+  for (const post of posts) {
+    postList.push(<Post post={post} />);
   }
-  return <RepsBoard>{rep_list}</RepsBoard>;
+  return <PostBoard>{postList}</PostBoard>;
 };
 
-const RepsBoard = styled.div`
+const PostBoard = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
 `;
 
-export default Reps;
+export default Posts;
