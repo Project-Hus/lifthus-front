dbf2c8faa519bcda380a45fe02844376b6afa8ec
jinha <75898490+iububble@users.noreply.github.com>
2023-06-02 12:40:39 +0900

    운동type(반복, 시간, 단순) 추가

--- src/pages/routine/CreateProgram/SearchExrcise.tsx
+++ src/pages/routine/CreateProgram/SearchExrcise.tsx
@@ -6,60 +6,59 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { exerciseDB, exerciseList } from "../../../api/mocks/routineApi.mock";
 
-
-
 type SearchFormData = {
-    searchTerm: string;
+  searchTerm: string;
 };
 
+const SearchExercise = ({
+  addExercise,
+}: {
+  addExercise: (excercise: exerciseDB) => void;
+}) => {
+  const [SearchResult, setSearchResult] = useState<exerciseDB[]>([]);
+  type SearchFormData = {
+    searchTerm: string;
+  };
 
+  const { register, handleSubmit } = useForm<SearchFormData>();
 
+  const onSubmit = () => {
+    setSearchResult(exerciseList);
+  };
 
+  const addExerciseHandler = (exercise: exerciseDB) => {
+    console.log("clicked");
+    addExercise(exercise);
+  };
 
-const SearchExercise = ({ addExercise }: { addExercise: (excercise: exerciseDB) => void }) => {
-    const [SearchResult, setSearchResult] = useState<exerciseDB[]>([]);
-    type SearchFormData = {
-        searchTerm: string;
-    };
-
-    const { register, handleSubmit } = useForm<SearchFormData>();
-
-    const onSubmit = () => {
-        setSearchResult(exerciseList);
-    }
-
-    const addExerciseHandler = (exercise: exerciseDB) => {
-        console.log("clicked")
-        addExercise(exercise);
-    }
-
-
-
-    return (
-        <>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <Input {...register('searchTerm')} type="text" placeholder="검색어를 입력하세요" />
-                <Button type="submit">검색</Button>
-            </form>
-            {SearchResult.length > 0 &&
-                SearchResult.map((exercise) => {
-                    return (
-                        <div key={exercise.id}>
-                            <Flex justifyContent={"space-between"}>
-                                <Img src={exercise.images[0]} width="10%" alt="exercise" />
-                                <Text>{exercise.name}</Text>
-                                <Text>{exercise.trainingType}</Text>
-                                <Text>{exercise.bodyPort ? exercise.bodyPort : "없음"}</Text>
-                                <Button onClick={() => addExerciseHandler(exercise)}><AddIcon /></Button>
-                            </Flex>
-                        </div>
-                    )
-                }
-                )
-            }
-
-        </>
-    )
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          {...register("searchTerm")}
+          type="text"
+          placeholder="검색어를 입력하세요"
+        />
+        <Button type="submit">검색</Button>
+      </form>
+      {SearchResult.length > 0 &&
+        SearchResult.map((exercise) => {
+          return (
+            <div key={exercise.id}>
+              <Flex justifyContent={"space-between"}>
+                <Img src={exercise.images[0]} width="10%" alt="exercise" />
+                <Text>{exercise.name}</Text>
+                <Text>{exercise.trainingType}</Text>
+                <Text>{exercise.bodyPart ? exercise.bodyPart : "없음"}</Text>
+                <Button onClick={() => addExerciseHandler(exercise)}>
+                  <AddIcon />
+                </Button>
+              </Flex>
+            </div>
+          );
+        })}
+    </>
+  );
 };
 
 export default SearchExercise;
