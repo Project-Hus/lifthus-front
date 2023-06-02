51f24a9b2bcb8f4d47c468767a8ae3acf619c834
jinha <75898490+iububble@users.noreply.github.com>
2023-06-02 19:02:07 +0900

    프로그램생성창 전체 data  위한 zustand

---
+++ src/store/interfaces/program.interface.ts
@@ -0,0 +1,35 @@
+import { exerciseDB, programDB } from "../../api/mocks/routineApi.mock";
+
+export interface programInfo {
+  id: number;
+  author: number;
+  date: Date;
+  created_at: Date;
+  updated_at: Date;
+  trainingType: string;
+  images?: string[];
+  description: string;
+  likenum: number;
+  starednum: number;
+  routineName: string;
+  timer?: number;
+  sets?: number;
+  weight?: number;
+  setProgramInfo: (info: programDB) => void;
+}
+export interface day {
+  dayname?: string;
+  exerciseList?: exerciseDB[];
+}
+export interface week {
+  days?: day[];
+}
+export interface plan {
+  weeks: week[];
+}
+export interface programPlanInfo {
+  programInfo: programDB;
+  plan: plan;
+  setProgramPlanInfo: (info: programDB) => void;
+  setWeekInfo: (weeks: week[]) => void;
+}
