3685be812cc1316b9401f21596943033272d2bc2
lifthus <lifthus531@gmail.com>
2023-06-06 20:02:14 +0900

    #69/feat/act api established

--- src/api/interfaces/programApi.interface.ts
+++ src/api/interfaces/programApi.interface.ts
@@ -2,6 +2,6 @@ import { CreateActDto, QueryActDto } from "../dtos/program/act.dto";
 
 // make commentapi interface function
 export interface ProgramApi {
-  queryActsByName: (name: string, skip: number) => Promise<QueryActDto[]>;
+  queryActsByName: (name: string, skip?: number) => Promise<QueryActDto[]>;
   createAct: (newAct: CreateActDto) => Promise<QueryActDto>;
 }
---
+++ src/api/testApi/programTestApi.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { LIFTHUS_API_URL } from "../../common/routes";
+import { CreateActDto } from "../dtos/program/act.dto";
+import { ProgramApi } from "../interfaces/programApi.interface";
+
+const programTestApi: ProgramApi = {
+  queryActsByName: async (name: string, skip?: number) => {
+    const res = await axios.get(LIFTHUS_API_URL + "/routine/act", {
+      params: {
+        name: name,
+        skip: skip || 0,
+      },
+    });
+    return res.data;
+  },
+  createAct: async (newAct: CreateActDto) => {
+    const lst = localStorage.getItem("lifthus_store");
+    const res = await axios.post(LIFTHUS_API_URL + "/routine/act", newAct, {
+      withCredentials: true,
+      headers: {
+        Authorization: lst,
+      },
+    });
+    return res.data;
+  },
+};
+
+export default programTestApi;
