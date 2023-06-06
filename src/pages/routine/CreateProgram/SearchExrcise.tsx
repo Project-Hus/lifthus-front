228557201e467ae5c9d18a02424a18b571922138
lifthus <lifthus531@gmail.com>
2023-06-06 22:14:24 +0900

    #69/feat/search act works

--- src/pages/routine/CreateProgram/SearchExrcise.tsx
+++ src/pages/routine/CreateProgram/SearchExrcise.tsx
@@ -1,16 +1,13 @@
-//검색 창 컴포넌트를 만들어줘야 함.
+import React, { useEffect, useState } from "react";
 import { AddIcon } from "@chakra-ui/icons";
 import { Button, Flex, Img, Input, Text } from "@chakra-ui/react";
-import styled from "@emotion/styled";
-import React, { FormEvent, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { exerciseList } from "../../../api/mocks/program.mock";
+import programApi from "../../../api/programApi";
 import { actDB } from "../../../store/interfaces/program.interface";
 import { useProgramPlanStore } from "../../../store/program.zustand";
-
-type SearchFormData = {
-  searchTerm: string;
-};
+import { cl, s } from "@fullcalendar/core/internal-common";
 
 const SearchExercise = ({
   weekNum,
@@ -19,11 +16,10 @@ const SearchExercise = ({
   weekNum: number;
   dayNum: number;
 }) => {
+  const { register, getValues } = useForm();
+
   const { setProgramPlanInfo, program } = useProgramPlanStore();
   const [SearchResult, setSearchResult] = useState<actDB[]>([]);
-  type SearchFormData = {
-    searchTerm: string;
-  };
 
   const [serachstring, setSerachstring] = useState<string>("");
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,28 +36,64 @@ const SearchExercise = ({
     setProgramPlanInfo({ acts: [...program.acts, newact] });
   };
 
+  // real time search logic
+  const [searchKeyword, setSearchKeyword] = useState<string>("");
+  const { data: queriedActs, isLoading } = useQuery(
+    ["search", "act", searchKeyword],
+    () => {
+      return programApi.queryActsByName(searchKeyword);
+    },
+    {
+      enabled: !!searchKeyword,
+    }
+  );
+  let TOK: NodeJS.Timeout = setTimeout(() => {}, 1000);
+  useEffect(() => () => clearTimeout(TOK), [TOK]);
   return (
     <>
       <Flex>
         <Input
           type="text"
           placeholder="검색어를 입력하세요"
-          onChange={handleSearch}
+          {...register("search", {
+            onChange: (e) => {
+              clearTimeout(TOK);
+              TOK = setTimeout(() => {
+                setSearchKeyword(getValues("search"));
+              }, 250);
+            },
+          })}
         />
         <Button type="button" onClick={onsubmit}>
           검색
         </Button>
       </Flex>
-      {SearchResult.length > 0 &&
-        SearchResult.map((exercise) => {
+      {queriedActs &&
+        queriedActs.map((act) => {
           return (
-            <div key={exercise.id}>
+            <div key={act.id}>
               <Flex justifyContent={"space-between"}>
-                <Img src={exercise.images[0]} width="10%" alt="exercise" />
-                <Text>{exercise.name}</Text>
-                <Text>{exercise.type}</Text>
-                <Text>{exercise.bodyPart ? exercise.bodyPart : "없음"}</Text>
-                <Button onClick={() => addExerciseHandler(exercise)}>
+                <Img
+                  src={
+                    act.image ||
+                    "https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-6.png"
+                  }
+                  width="10%"
+                  alt="exercise"
+                />
+                <Text>{act.name}</Text>
+                <Text>
+                  {(act.type == "rep" && "반복") ||
+                    (act.type == "lap" && "시간")}
+                </Text>
+                <Text>
+                  {act.upper && act.lower
+                    ? "전신"
+                    : act.upper
+                    ? "상체"
+                    : "하체"}
+                </Text>
+                <Button onClick={() => {}}>
                   <AddIcon />
                 </Button>
               </Flex>
