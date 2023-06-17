2af2fccf7c0e60dbe572fd5ea48169f7457d11a3
lifthus <lifthus531@gmail.com>
2023-06-17 15:49:41 +0900

    cookie test button in profile setting

--- src/components/profile/profileTab/ProfileSetting.tsx
+++ src/components/profile/profileTab/ProfileSetting.tsx
@@ -20,6 +20,11 @@ const ProfileSetting = () => {
   return (
     <div>
       <Button onClick={() => signOut()}>Sign out</Button>
+      <Button
+        onClick={() => {
+          window.location.href = "https://auth.cloudhus.com/auth/test/cookie";
+        }}
+      ></Button>
     </div>
   );
 };
