242ed9917209a1a7fade5f4cb9ae471a0a00d14b
lifthus <lifthus531@gmail.com>
2023-08-12 22:32:59 +0900

    #106/imp/Register form validations

--- src/components/register/RegisterNumber.tsx
+++ src/components/register/RegisterNumber.tsx
@@ -2,8 +2,10 @@ import { Image } from "@chakra-ui/react";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 import FormInput from "../../common/components/forms/FormInput";
 import BlueLink from "../../common/components/links/BlueLink";
+import SubmitLink from "../../common/components/links/SubmitLink";
 import Logo from "../../common/components/Logo";
 import { RegisterInfoStrings } from "../../store/interfaces/register.interface";
 import useRegisterStore from "../../store/register.zustand";
@@ -24,8 +26,8 @@ const RegisterNumber = ({
   pref?: any;
   unit: string;
   next: string;
-  min?: number;
-  max?: number;
+  min: number;
+  max: number;
   img?: string;
   alt?: string;
 }) => {
@@ -38,6 +40,12 @@ const RegisterNumber = ({
     shouldUseNativeValidation: true,
     defaultValues: { value: register_value },
   });
+
+  const navigate = useNavigate();
+
+  const val = getValues("value");
+  const isValid = val >= min && val <= max;
+
   return (
     <>
       <Logo></Logo>
@@ -45,26 +53,32 @@ const RegisterNumber = ({
         {img && <Image src={img} alt={alt} sx={{ width: "20vw" }} />}
       </div>
       <p>{content}</p>
-      <div style={{ display: "flex" }}>
-        <FormInput
-          type="number"
-          pref={pref}
-          unit={unit}
-          bold={true}
-          {...register("value", {
-            min: min,
-            max: max,
-            onChange: (e) => {
-              const tmp: any = {};
-              tmp[take] = Number(getValues("value"));
-              registerNumber(tmp);
-            },
-          })}
-        />
-      </div>
-      <p>
-        <BlueLink to={next}>{t("Next")}</BlueLink>{" "}
-      </p>
+      <form
+        onSubmit={() => {
+          navigate(next);
+        }}
+      >
+        <div style={{ display: "flex" }}>
+          <FormInput
+            autoFocus={true}
+            type="number"
+            pref={pref}
+            unit={unit}
+            bold={true}
+            {...register("value", {
+              min: min,
+              max: max,
+              onChange: (e) => {
+                const tmp: any = {};
+                tmp[take] = Number(getValues("value"));
+                registerNumber(tmp);
+              },
+            })}
+          />
+        </div>
+        {isValid && <SubmitLink>{t("Next")}</SubmitLink>}
+      </form>
+      <p></p>
     </>
   );
 };
