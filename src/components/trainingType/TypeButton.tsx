50a91f00edd67d3d31ca4b4e21b5b9eb5ee43c88
lifthus <lifthus531@gmail.com>
2023-08-12 21:26:40 +0900

    #106/imp/all types got their own representative images

--- src/components/trainingType/TypeButton.tsx
+++ src/components/trainingType/TypeButton.tsx
@@ -55,9 +55,16 @@ const typeImages: { [key: string]: string } = {
     "https://pngimg.com/uploads/powerlifting/powerlifting_PNG44.png",
   bodybuilding: "https://cdn-icons-png.flaticon.com/512/30/30939.png",
   crossfit:
-    "https://images.vexels.com/media/users/3/154715/isolated/lists/f1b51578068934eb07276b95baa13c90-pull-up-crossfit-silhouette.png",
+    "https://creazilla-store.fra1.digitaloceanspaces.com/silhouettes/64625/kettlebell-silhouette-000000-md.png",
   weightlifting:
     "https://i0.wp.com/physicalculturestudy.com/wp-content/uploads/2018/04/olympics-944950_960_720.png?resize=297%2C425&ssl=1",
+  bodyweight:
+    "https://images.vexels.com/media/users/3/154715/isolated/lists/f1b51578068934eb07276b95baa13c90-pull-up-crossfit-silhouette.png",
+  cardio:
+    "https://cdn.pixabay.com/photo/2018/03/04/22/43/silhouette-3199472_1280.png",
+  etc: "https://cdn.pixabay.com/photo/2018/02/25/12/31/crossfit-3180368_1280.png",
+  undefined:
+    "https://creazilla-store.fra1.digitaloceanspaces.com/silhouettes/3509519/question-mark-silhouette-000000-md.png",
 };
 
 interface TypeButtonProps {
