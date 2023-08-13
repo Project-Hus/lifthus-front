f46d0f65ed599aa7e26fc5c643cbc37e06ae021f
lifthus <lifthus531@gmail.com>
2023-08-14 00:14:31 +0900

    #108/imp/Sign page fixed

--- src/pages/sign/Sign.tsx
+++ src/pages/sign/Sign.tsx
@@ -13,6 +13,8 @@ import {
 } from "../../common/routes";
 import { useQuery } from "@tanstack/react-query";
 import authApi from "../../api/authApi";
+import { Route, Routes } from "react-router-dom";
+import FlexCenterLayout from "../../common/components/layouts/FlexCenterLayout";
 
 const Sign = () => {
   const { t, i18n } = useTranslation();
@@ -36,33 +38,44 @@ const Sign = () => {
 
   // canceling last commit : git reset --soft HEAD~1
   return (
-    <GoogleOAuthProvider clientId="1028507845637-07t65vf8fs49o4dpaelvefgbj8ov56pn.apps.googleusercontent.com">
-      <Logo mov={true} absolute={true} />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      {isLoading ? (
-        <BlueSpinner />
-      ) : (
-        <GoogleLogin
-          text="continue_with"
-          ux_mode="redirect"
-          login_uri={
-            HUS_GOOGLE_LOGIN_ENDPOINT +
-            `?redirect=${encodeURIComponent(LIFTHUS_FRONT_URL)}&csid=${csid}`
-          }
-          onSuccess={() => {}}
-          auto_select={true}
-        />
-      )}
-      <br />
-      {/*
+    <FlexCenterLayout>
+      <Routes>
+        <Route
+          index
+          element={
+            <GoogleOAuthProvider clientId="1028507845637-07t65vf8fs49o4dpaelvefgbj8ov56pn.apps.googleusercontent.com">
+              <Logo mov={true} absolute={true} />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              {isLoading ? (
+                <BlueSpinner />
+              ) : (
+                <GoogleLogin
+                  text="continue_with"
+                  ux_mode="redirect"
+                  login_uri={
+                    HUS_GOOGLE_LOGIN_ENDPOINT +
+                    `?redirect=${encodeURIComponent(
+                      LIFTHUS_FRONT_URL
+                    )}&csid=${csid}`
+                  }
+                  onSuccess={() => {}}
+                  auto_select={true}
+                />
+              )}
+              <br />
+              {/*
       <BlueLink to="/sign/in">{t("sign.SignIn")}</BlueLink>
       <BlueLink to="/sign/up">{t("sign.SignUp")}</BlueLink>
         */}
-    </GoogleOAuthProvider>
+            </GoogleOAuthProvider>
+          }
+        />
+      </Routes>
+    </FlexCenterLayout>
   );
 };
 
