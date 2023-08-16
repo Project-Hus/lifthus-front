c640ad926340ffb7150ef299d608e5bbceff87be
lifthus <lifthus531@gmail.com>
2023-08-16 19:55:33 +0900

    #93/imp/ProfileTab refactored

---
+++ src/components/profile/profileTab/ProfileFigure.tsx
@@ -0,0 +1,135 @@
+import {
+  FormLabel,
+  Stat,
+  StatArrow,
+  StatGroup,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+  Text,
+} from "@chakra-ui/react";
+import { ThemeColor } from "../../../common/styles/theme.style";
+
+const ProfileFigure = () => {
+  return (
+    <>
+      {" "}
+      <FormLabel textAlign={"center"} fontSize="1em" fontWeight={"bold"}>
+        BIG 3
+      </FormLabel>
+      <StatGroup
+        border={`ridge 0.1em ${ThemeColor.backgroundColor}`}
+        borderRadius="1em"
+        padding="0.5em"
+      >
+        <Stat>
+          <StatLabel>Squat</StatLabel>
+          <StatNumber>
+            {160}
+            <Text display={"inline"} fontSize="0.5em">
+              kg
+            </Text>
+          </StatNumber>
+          <StatHelpText>
+            <StatArrow type="increase" />
+            23.36%
+          </StatHelpText>
+        </Stat>
+
+        <Stat>
+          <StatLabel>Benchpress</StatLabel>
+          <StatNumber>
+            {120}
+            <Text display={"inline"} fontSize="0.5em">
+              kg
+            </Text>
+          </StatNumber>
+          <StatHelpText>
+            <StatArrow type="decrease" />
+            9.05%
+          </StatHelpText>
+        </Stat>
+        <Stat>
+          <StatLabel>Deadlift</StatLabel>
+          <StatNumber>
+            {180}
+            <Text display={"inline"} fontSize="0.5em">
+              kg
+            </Text>
+          </StatNumber>
+          <StatHelpText>
+            <StatArrow type="decrease" />
+            9.05%
+          </StatHelpText>
+        </Stat>
+        <Stat>
+          <StatLabel>
+            <Text>Total</Text>
+          </StatLabel>
+          <StatNumber>
+            {460}
+            <Text display={"inline"} fontSize="0.5em">
+              kg
+            </Text>
+          </StatNumber>
+          <StatHelpText>
+            <StatArrow type="decrease" />
+            9.05%
+          </StatHelpText>
+        </Stat>
+      </StatGroup>
+      <FormLabel textAlign={"center"} fontSize="1em" fontWeight={"bold"}>
+        Body
+      </FormLabel>
+      <StatGroup
+        border={`ridge 0.1em ${ThemeColor.backgroundColor}`}
+        borderRadius="1em"
+        padding="0.5em"
+      >
+        <Stat>
+          <StatLabel>Height</StatLabel>
+          <StatNumber>
+            {183}
+            <Text display={"inline"} fontSize="0.5em">
+              cm
+            </Text>
+          </StatNumber>
+          <StatHelpText>
+            <StatArrow type="decrease" />
+            9.05%
+          </StatHelpText>
+        </Stat>
+        <Stat>
+          <StatLabel>Weight</StatLabel>
+          <StatNumber>
+            {105}
+            <Text display={"inline"} fontSize="0.5em">
+              kg
+            </Text>
+          </StatNumber>
+          <StatHelpText>
+            <StatArrow type="decrease" />
+            9.05%
+          </StatHelpText>
+        </Stat>
+        <Stat>
+          <StatLabel>
+            <Text>Fat Percentage</Text>
+          </StatLabel>
+          <StatNumber>
+            {18}
+            <Text display={"inline"} fontSize="0.5em">
+              %
+            </Text>
+          </StatNumber>
+          <StatHelpText>
+            <StatArrow type="decrease" />
+            9.05%
+          </StatHelpText>
+        </Stat>
+      </StatGroup>
+    </>
+  );
+};
+
+export default ProfileFigure;
---
+++ src/components/profile/profileTab/ProfileHeatmap.tsx
@@ -0,0 +1,48 @@
+import { Avatar } from "@chakra-ui/react";
+import ReactCalendarHeatmap from "react-calendar-heatmap";
+import { ThemeColor } from "../../../common/styles/theme.style";
+import "./calendar.css";
+
+const ProfileHeatmap = () => {
+  return (
+    <>
+      <ReactCalendarHeatmap
+        startDate={new Date(new Date().setMonth(new Date().getMonth() - 9))}
+        endDate={new Date()}
+        monthLabels={[]}
+        values={[
+          { date: "2023-01-02", count: 2 },
+          { date: "2023-01-03", count: 3 },
+          { date: "2023-01-04", count: 4 },
+          { date: "2023-01-05", count: 4 },
+          { date: "2023-01-06", count: 2 },
+          { date: "2023-01-07", count: 4 },
+          { date: "2023-01-08", count: 4 },
+          { date: "2023-01-09", count: 4 },
+        ]}
+        classForValue={(value) => {
+          if (!value) {
+            return "color-empty";
+          }
+          return `color-scale-${value.count}`;
+        }}
+      />
+      <div>
+        <Avatar
+          margin={"0.2em"}
+          name={"Powerlifter"}
+          bgColor={ThemeColor.basicColor}
+          src={"https://pngimg.com/uploads/powerlifting/powerlifting_PNG44.png"}
+          sx={{
+            "@media screen and (max-width: 350px)": {
+              w: "2em",
+              h: "2em",
+            },
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+export default ProfileHeatmap;
