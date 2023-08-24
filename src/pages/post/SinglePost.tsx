ce0d65b7abb950130f1c1c621a2b709655a4f0ae
lifthus <lifthus531@gmail.com>
2023-08-24 16:26:55 +0900

    #127/-

--- src/pages/post/SinglePost.tsx
+++ src/pages/post/SinglePost.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import PostV2 from "../../components/posts/Post";
+import Post from "../../components/posts/Post";
 
 const SinglePost = () => {
   const slug = useParams().slug;
   return (
     <div>
-      <PostV2 slug={slug} />
+      <Post slug={slug} />
     </div>
   );
 };
